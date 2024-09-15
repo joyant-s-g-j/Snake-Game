3b473890b66b2fc047f065fce3b96056a29873a4
joyantgupta <joyantsheikharguptajoy@gmail.com>
2024-09-16 04:18:33 +0600

    fixing the body part issue

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -104,6 +104,22 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener{
             placeFood();
         }
 
+        for(int i=snakeBody.size()-1;i>=0; i--)
+        {
+            Tile snakePart = snakeBody.get(i);
+            if(i == 0)
+            {
+                snakePart.x = snakeHead.x;
+                snakePart.y = snakeHead.y;
+            }
+            else
+            {
+                Tile prevSnakePart = snakeBody.get(i-1);
+                snakePart.x = prevSnakePart.x;
+                snakePart.y = prevSnakePart.y;
+            }
+        }
+
         snakeHead.x += velocityX;
         snakeHead.y += velocityY;
     }
